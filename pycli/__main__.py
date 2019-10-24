3c6be72c262ec957e9e0a4f88f51e1a6ea9dbb03
Akshith Gunasekaran <akshith.573@gmail.com>
2019-10-24 12:11:20 -0700

    argparse

--- pycli/__main__.py
+++ pycli/__main__.py
@@ -1,19 +1,22 @@
-import sys
+import argparse
 from .classmodule import MyClass
 from .funcmodule import my_function
 
 
+parser = argparse.ArgumentParser()
+parser.add_argument('-x', type=int, default=0,
+                    help='set value of x')
+args = parser.parse_args()
+
+
 def main():
-    print('in main')
-    args = sys.argv[1:]
-    print('count of args :: {}'.format(len(args)))
-    for arg in args:
-        print('passed argument :: {}'.format(arg))
+    print('x = {}'.format(args.x))
 
     my_function('hello world')
 
     my_object = MyClass('G')
     my_object.say_name()
+
 
 if __name__ == '__main__':
     main()
